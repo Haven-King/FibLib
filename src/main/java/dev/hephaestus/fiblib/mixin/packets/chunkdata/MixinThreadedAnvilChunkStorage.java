f60504907149f13b043296a226ab5f5db34011cf
Draylar <17728338+Draylar@users.noreply.github.com>
2022-01-22 12:52:49 -0600

    Fix ThreadedAnvilChunkStorage mixin parameters

--- src/main/java/dev/hephaestus/fiblib/mixin/packets/chunkdata/MixinThreadedAnvilChunkStorage.java
+++ src/main/java/dev/hephaestus/fiblib/mixin/packets/chunkdata/MixinThreadedAnvilChunkStorage.java
@@ -2,9 +2,11 @@ package dev.hephaestus.fiblib.mixin.packets.chunkdata;
 
 import dev.hephaestus.fiblib.impl.FibLib;
 import net.minecraft.network.Packet;
+import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
 import net.minecraft.world.chunk.WorldChunk;
+import org.apache.commons.lang3.mutable.MutableObject;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ThreadedAnvilChunkStorage.class)
 public abstract class MixinThreadedAnvilChunkStorage {
     @Inject(method = "sendChunkDataPackets", at = @At(value = "NEW", target = "net/minecraft/network/packet/s2c/play/ChunkDataS2CPacket"))
-    public void fixPackets(ServerPlayerEntity player, Packet<?>[] packets, WorldChunk chunk, CallbackInfo ci) {
+    public void fixPackets(ServerPlayerEntity player, MutableObject<ChunkDataS2CPacket> cachedDataPacket, WorldChunk chunk, CallbackInfo ci) {
         FibLib.PLAYER.set(player);
     }
 }
